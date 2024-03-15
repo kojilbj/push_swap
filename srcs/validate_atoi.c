0a51b8686b0c4b43594e83746f8eb6a88fc254ea
渡辺耕次 <watanabekoji@sekainihitotsudakenoMacBook-Air.local>
2024-03-16 00:15:45 +0900

    fix on '0' error

--- srcs/validate_atoi.c
+++ srcs/validate_atoi.c
@@ -6,13 +6,13 @@
 /*   By: kojwatan <kojwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 23:54:38 by kojwatan          #+#    #+#             */
-/*   Updated: 2024/03/15 23:48:58 by kojwatan         ###   ########.fr       */
+/*   Updated: 2024/03/16 00:10:38 by kojwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static long	validate_strtol(const char *str, int sign)
+static long	validate_strtol(const char *str, int sign_fg)
 {
 	int	result;
 	int	tmp;
@@ -24,7 +24,7 @@ static long	validate_strtol(const char *str, int sign)
 		result = result * 10 + *str - '0';
 		if (result < tmp)
 		{
-			if (sign == -1)
+			if (sign_fg == -1)
 				return (-1);
 			else
 				return (-1);
@@ -33,28 +33,26 @@ static long	validate_strtol(const char *str, int sign)
 	}
 	if (*str != '\0' && *str != ' ')
 		return (-1);
-	if (result == 0)
+	if (result == 0 && sign_fg == 1)
 		return (-1);
 	return (1);
 }
 
 int	validate_atoi(const char *str)
 {
-	int		sign;
 	int		sign_fg;
 
 	sign_fg = 0;
-	sign = 1;
+	if (*str == '\0')
+		return (1);
 	while (*str && ft_isdigit(*str) == 0)
 	{
 		if (validate_char(*str) == -1)
 			return (-1);
-		if (*str == '-')
-			sign *= -1;
 		if (sign_fg == 1)
 			return (-1);
 		sign_fg = 1;
 		str++;
 	}
-	return ((int)(validate_strtol(str, sign)));
+	return ((int)(validate_strtol(str, sign_fg)));
 }
